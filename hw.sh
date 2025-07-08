29ea6977b7b9bfa600d2502644918b0cc96cdf58
jim@vheavy.com <jim@vheavy.com>
2025-07-08 12:15:16 -0700

    Add commands to hw.sh to power cycle the Atari

--- hw.sh
+++ hw.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # Build and run on hardware
-cp core1.cpp stash/core1.cpp.`date +%Y%m%d.%H%M%S`   && make PORT=/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_30\:ED\:A0\:A8\:D7\:A8-if00 uc
+PORT=/dev/serial/by-id/usb-Espressif_USB_JTAG_serial_debug_unit_30\:ED\:A0\:A8\:D7\:A8-if00
+mosquitto_pub -h 192.168.68.137 -t cmnd/tasmota_71D51D/POWER -m OFF 
+cp core1.cpp stash/core1.cpp.`date +%Y%m%d.%H%M%S`
+make PORT=${PORT} upload
+( sleep 3 && mosquitto_pub -h 192.168.68.137 -t cmnd/tasmota_71D51D/POWER -m ON ) &
+make PORT=${PORT} cat | cat_until DONE
+
 
